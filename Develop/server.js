7c6449c42e5a9f5492c19bda80ad29ff343a0b1d
DJ Hersh <DanielJHersh@gmail.com>
2020-11-30 17:57:14 -0800

    Created path to index html file

--- Develop/server.js
+++ Develop/server.js
@@ -12,6 +12,10 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
 });
