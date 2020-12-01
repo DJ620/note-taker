ff8ec7330e3ceded0e6184da9888f3662450a3a7
DJ Hersh <DanielJHersh@gmail.com>
2020-11-30 20:50:29 -0800

    Have my post function appending data to the json file, but giving me errors that I still need to figure out

--- Develop/server.js
+++ Develop/server.js
@@ -16,12 +16,26 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+const getData = fs.readFile(__dirname + "/db/db.json", (err, data) => {
+    if (err) throw err;
+    return data;
+});
+
 app.get("/api/notes", (req, res) => {
     fs.readFile(__dirname + "/db/db.json", (err, data) => {
         if (err) throw err;
         res.end(data);
     });
 });
+
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    fs.appendFile(__dirname + "/db/db.json", JSON.stringify(newNote), (err) => {
+        if (err) throw err;
+        console.log("Note added!");
+        res.end(newNote);
+    })
+})
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
