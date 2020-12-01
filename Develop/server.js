f249e5b568659f024af24fd1ded396314b6518ab
DJ Hersh <DanielJHersh@gmail.com>
2020-11-30 18:25:28 -0800

    Created the path to get the data stored in the db.json file

--- Develop/server.js
+++ Develop/server.js
@@ -16,6 +16,13 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
+app.get("/api/notes", (req, res) => {
+    fs.readFile(__dirname + "/db/db.json", (err, data) => {
+        if (err) throw err;
+        res.end(data);
+    });
+});
+
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
 });
