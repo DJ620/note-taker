c38357a4d8ea7e37d6e0fa8bd049891e8dbff183
DJ Hersh <DanielJHersh@gmail.com>
2020-12-02 18:36:41 -0800

    Required the db.json file which fixed my problem of the notes not showing up on the page

--- server.js
+++ server.js
@@ -1,48 +1,45 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const db = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Tells the server to serve all files in the 'public' file
 app.use(express.static("public"));
-
-let notesArr = [];
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-const getData = fs.readFile(__dirname + "/db/db.json", (err, data) => {
-    if (err) throw err;
-    notesArr = [];
-    notesArr.push(...JSON.parse(data));
-    console.log(notesArr);
 });
 
 app.get("/api/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
+let noteID = db.length;
 app.post("/api/notes", (req, res) => {
+    noteID++;
     const newNote = req.body;
+    newNote.id = noteID;
     console.log(newNote);
-    getData;
-    notesArr.push(newNote);
-    fs.writeFile("db/db.json", JSON.stringify(notesArr), (err) => 
+    db.push(newNote);
+    fs.writeFile("db/db.json", JSON.stringify(db), (err) => 
         err ? console.error(err) : console.log("Added new note!")
     );
-    res.end(JSON.stringify(notesArr));
+    res.end(JSON.stringify(db));
+});
+
+app.delete("api/notes/:id", (req, res) => {
+    console.log(req.params.id);
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, () => {
     console.log("App listening on PORT " + PORT);
 });
-
-console.log(__dirname);
